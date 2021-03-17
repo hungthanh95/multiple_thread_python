0b602b82c934303c5509545f2c60cd8d121f94a6
hungthanh95 <hungthanh95@gmail.com>
2021-03-17 16:51:28 +0700

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# main.py - Downloads XKCD comics using multiple threads.
+
+import requests, os, bs4, threading
+
+os.makedirs('xkcd', exist_ok=True)  # store comics in ./xkcd
+
+
+def download_xkcd(start_comic, end_comic):
+    for url_number in range(start_comic, end_comic):
+        # Download the page.
+        print('Downloading page http://xkcd.com/%s...' % url_number)
+        res = requests.get('http://xkcd.com/%s' % url_number)
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text)
+
+        # Find the URL of the comic image.
+        comic_elem = soup.select('#comic img')
+        if not comic_elem:
+            print('Could not find comic image.')
+        else:
+            comic_url = comic_elem[0].get('src')
+
+        # Download the image.
+        print('Downloading image %s...' % comic_url)
+        res = requests.get(comic_url)
+        res.raise_for_status()
+        # Save the image to ./xkcd.
+        with open(os.path.join('xkcd', os.path.basename(comic_url)), 'wb') as image_file:
+            for chunk in res.iter_content(100000):
+                image_file.write(chunk)
+
+
+# TODO: Create and start the Thread objects.
+# TODO: Wait for all threads to end.
